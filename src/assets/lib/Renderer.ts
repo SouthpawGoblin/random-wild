ebc4f4da491677e8d7bb2592b940419437df4ff9
Yinzhe_Qi <qiyzh@neusoft.com>
2019-11-20 18:08:44 +0800

    feat: add classes

---
+++ src/assets/lib/Renderer.ts
@@ -0,0 +1,30 @@
+import { Vec2 } from "./types"
+
+export default class Renderer {
+
+  ctx: CanvasRenderingContext2D
+  pixelRatio: number
+  zoom: number = 1
+  angle: number = 0
+  renderFunc: (ctx: CanvasRenderingContext2D) => void = () => { }
+  
+  constructor(canvas: HTMLCanvasElement, pixelRatio: number) {
+    const context = canvas.getContext('2d')
+    if (!context) {
+      throw new Error('can not aquire canvas context!')
+    }
+    this.ctx = context
+    this.pixelRatio = pixelRatio
+  }
+
+  render() {
+    this.renderFunc(this.ctx)
+    requestAnimationFrame(this.render)
+  }
+
+  private world2canvas(pos: Vec2): Vec2 {
+    const x = pos.x * this.zoom * this.pixelRatio + this.ctx.canvas.width / 2
+    const y = pos.y * this.zoom * this.pixelRatio + this.ctx.canvas.height / 2
+    return { x, y }
+  }
+}
